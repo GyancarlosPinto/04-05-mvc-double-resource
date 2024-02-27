dd341c63fe51004f31c0533bafbf8856566e8b95
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-02-27 00:01:37 -0500

    Finished Data Problems

--- data/todos.js
+++ data/todos.js
@@ -1,24 +1,33 @@
 let todos = [];
+let nextTodoId = 1;
 
 /**
  * Finds and returns all todo items.
  * @returns {Array} An array of all todo items.
  */
-const findAll = () => {};
+const findAll = () => {
+    return todos;
+};
 
 /**
  * Finds a todo item by its ID.
  * @param {number|string} id The ID of the todo item to find.
  * @returns {Object|null} The found todo item or null if not found.
  */
-const findById = (id) => {};
+const findById = (id) => {
+    return todos.find((todo) => todo.id === Number(id));
+};
 
 /**
  * Adds a new todo item.
  * @param {Object} todo The todo item to add. Must contain `title` and `completed` properties.
  * @returns {Object} The added todo item, including its generated ID.
  */
-const add = (todo) => {};
+const add = (todo) => {
+    const newToDo = { ...todo, id: nextTodoId++ };
+    todos.push(newToDo);
+    return newToDo;
+};
 
 /**
  * Updates a todo item by its ID.
@@ -26,13 +35,22 @@ const add = (todo) => {};
  * @param {Object} updateInfo An object containing the todo item properties to update.
  * @returns {Object|null} The updated todo item or null if not found.
  */
-const updateById = (id, updateInfo) => {};
+const updateById = (id, updateInfo) => {
+    const index = todos.findIndex(todo => todo.id === Number(id));
+    if(index === -1) return null;
+    todos[index] = { ...todos[index], ...updateInfo };
+    return todos[index];
+};
 
 /**
  * Deletes a todo item by its ID.
  * @param {number|string} id The ID of the todo item to delete.
  * @returns {Object|null} The deleted todo item or null if not found.
  */
-const deleteById = (id) => {};
+const deleteById = (id) => {
+    const index = todos.findIndex(todo => todo.id === Number(id));
+    if (index === -1) return null;
+    return todos.splice(index, 1)[0];
+};
 
 module.exports = { findAll, findById, add, updateById, deleteById };
--- data/users.js
+++ data/users.js
@@ -1,24 +1,33 @@
 let users = [];
+let nextUserId = 1;
 
 /**
  * Finds and returns all user items.
  * @returns {Array} An array of all user items.
  */
-const findAll = () => {};
+const findAll = () => {
+    return users;
+};
 
 /**
  * Finds a user item by its ID.
  * @param {number|string} id The ID of the user item to find.
  * @returns {Object|null} The found user item or null if not found.
  */
-const findById = (id) => {};
+const findById = (id) => {
+    return users.find((user) => user.id === id);
+};
 
 /**
  * Adds a new user item.
  * @param {Object} user The user item to add. Must contain `name` and `email` properties.
  * @returns {Object} The added user item, including its generated ID.
  */
-const add = (user) => {};
+const add = (user) => {
+    const newUser = { ...user, id: nextUserId++ };
+    users.push(newUser);
+    return newUser;
+};
 
 /**
  * Updates a user item by its ID.
@@ -26,13 +35,22 @@ const add = (user) => {};
  * @param {Object} updateInfo An object containing the user item properties to update.
  * @returns {Object|null} The updated user item or null if not found.
  */
-const updateById = (id, updateInfo) => {};
+const updateById = (id, updateInfo) => {
+    const index = users.findIndex(user => user.id === Number(id));
+    if (index === -1) return null;
+    users[index] = { ...users[index], ...updateInfo };
+    return users[index];
+};
 
 /**
  * Deletes a user item by its ID.
  * @param {number|string} id The ID of the user item to delete.
  * @returns {Object|null} The deleted user item or null if not found.
  */
-const deleteById = (id) => {};
+const deleteById = (id) => {
+    const index = users.findIndex(user => user.id === Number(id));
+    if (index === -1) return null;
+    return users.splice(index, 1)[0];
+};
 
 module.exports = { findAll, findById, add, updateById, deleteById };
